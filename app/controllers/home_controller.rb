0761960da6744b6c005187dddedc415b641836f0
qwerqy <amnrsln@gmail.com>
2018-11-05 21:22:25 +0800

    adjusted home controller and added search controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
   def index
-    @surveys = Survey.all
+    @questions = Question.includes(:answers).all
+  end
+
+  def search
+    if params[:search]
+      search = PgSearch.multisearch(params[:search])
+      @questions = render json: search
+    else
+      @questions = render json: Question.includes(:answers).all
+    end
   end
 end
