e06eb38db20761a66aa93528432fed2b28d18d44
qwerqy <amnrsln@gmail.com>
2018-11-05 21:23:48 +0800

    other changes

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2018_11_05_031242) do
     t.index ["question_id"], name: "index_answers_on_question_id"
   end
 
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id"
+  end
+
   create_table "questions", force: :cascade do |t|
     t.string "qid"
     t.string "title"
