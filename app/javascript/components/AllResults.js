e21913fe1c35eeeb818f6a07c9d643c9a4f7723d
qwerqy <amnrsln@gmail.com>
2018-11-05 22:01:04 +0800

    refactored results page

--- app/javascript/components/AllResults.js
+++ app/javascript/components/AllResults.js
@@ -1,37 +1,37 @@
-import React from "react"
-import { Grid, Segment, Header } from 'semantic-ui-react'
+import React from "react";
+import { List, Grid, Segment, Header } from "semantic-ui-react";
 class AllResults extends React.Component {
-  render () {
-    const { filteredQuestions } = this.props
+  render() {
+    const { filteredQuestions } = this.props;
     return (
       <div>
         <Grid stackable columns={2}>
           {Object.keys(filteredQuestions).map(key => {
-                return (
-                  <Grid.Column key={key}>
-                    <Segment>
-                      <Header as='p' block>{filteredQuestions[key].survey.name}</Header>
-                      <Header>
-                        <Header.Subheader>{filteredQuestions[key].qid}</Header.Subheader>
-                        {filteredQuestions[key].title}
-                      </Header>
-                      <ul>
-                        { filteredQuestions[key].answers.map(answer => {
-                          return (
-                            <li>{answer.input}</li>
-                          )
-                        })
-                      }
-                    </ul>
-                  </Segment>
-                </Grid.Column>
-                )
-            })
-          }
+            return (
+              <Grid.Column key={key}>
+                <Segment>
+                  <Header as="p" block>
+                    {filteredQuestions[key].survey.name}
+                  </Header>
+                  <Header>
+                    <Header.Subheader>
+                      {filteredQuestions[key].qid}
+                    </Header.Subheader>
+                    {filteredQuestions[key].title}
+                  </Header>
+                  <List animated>
+                    {filteredQuestions[key].answers.map(answer => {
+                      return <List.Item key={answer.id}>{answer.input}</List.Item>;
+                    })}
+                  </List>
+                </Segment>
+              </Grid.Column>
+            );
+          })}
         </Grid>
       </div>
     );
   }
 }
 
-export default AllResults
+export default AllResults;
--- app/javascript/components/SearchResults.js
+++ app/javascript/components/SearchResults.js
@@ -1,32 +1,32 @@
 import React from "react"
-import { Grid, Segment, Header } from 'semantic-ui-react'
+import { List, Grid, Segment, Header } from 'semantic-ui-react'
 class SearchResults extends React.Component {
   render () {
     const { results } = this.props
     return (
       <div>
-        {Object.keys(results).map(key => {
-            return (
-              <Grid.Column key={key}>
-                <Segment>
-                  <Header as='p' block>{results[key].survey.name}</Header>
-                  <Header>
-                    <Header.Subheader>{results[key].qid}</Header.Subheader>
-                    {results[key].title}
-                  </Header>
-                  <ul>
-                    { results[key].answers.map(answer => {
-                      return (
-                        <li>{answer.input}</li>
-                      )
-                    })
-                  }
-                </ul>
-              </Segment>
-            </Grid.Column>
-            )
-          })
-        }
+        <Grid stackable columns={2}>
+          {Object.keys(results).map(key => {
+              return (
+                <Grid.Column key={key}>
+                  <Segment>
+                    <Header as='p' block>{results[key].survey.name}</Header>
+                    <Header>
+                      <Header.Subheader>{results[key].qid}</Header.Subheader>
+                      {results[key].title}
+                    </Header>
+                    <List animated>
+                      { results[key].answers.map(answer => {
+                        return <List.Item key={answer.id}>{answer.input}</List.Item>
+                        })
+                      }
+                    </List>
+                </Segment>
+              </Grid.Column>
+              )
+            })
+          }
+        </Grid>
       </div>
     );
   }
