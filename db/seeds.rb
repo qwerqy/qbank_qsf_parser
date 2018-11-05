32cf33f186b90b9d14257750b51d0cb998de38ec
qwerqy <amnrsln@gmail.com>
2018-11-05 12:46:46 +0800

    seeding db with qsf files

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+surveys = %w(Coffee_Incidence_Check FB_Incidence_Check_V2 Travel_Survey_Trends_Internal IR_Check_for_Roaming_Travel_Study Online_Shopping_-_Copy Property_N9_Incidence_Check Radio_Listeners_Incidence_Test)
+
+surveys.each do |s|
+  # File is parsed into json..
+  survey = JSON.parse(File.read("app/assets/javascripts/qsf/#{s}.qsf"))
+  # Survey details extracted out into database.
+  inSurvey = Survey.create(sid: survey['SurveyEntry']['SurveyID'], name: survey['SurveyEntry']['SurveyName'])
+
+  # Selects all elements with element 'SQ'..
+  q = survey['SurveyElements'].select {|a| a['Element'] ==  'SQ'}
+
+  # iterate through all the 'SQ' elements..
+  q.each do |question|
+
+    # Creates the question from the element..
+    inQuestion = Question.create(qid: question['PrimaryAttribute'], title: question['SecondaryAttribute'], survey_id: inSurvey.id)
+
+    # Some questions have empty fields, condition to separate goes here..
+    if question['Payload']['Choices'].nil?
+      Answer.create(input: '', question_id: inQuestion.id)
+    else
+      question['Payload']['Choices'].each do |answer|
+        Answer.create(input: answer.last['Display'], question_id: inQuestion.id)
+      end
+    end
+  end
+end
