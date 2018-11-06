1515ad40b2d8ed2ddc10d13246375780b58b0dcf
qwerqy <amnrsln@gmail.com>
2018-11-06 11:37:23 +0800

    upload and parse logic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,4 +12,31 @@ class HomeController < ApplicationController
       @questions = render json: Question.includes(:answers).all
     end
   end
+
+  def upload
+    file = params[:qsf].read
+    survey = JSON.parse(file)
+    # Survey details extracted out into database.
+    inSurvey = Survey.create(sid: survey['SurveyEntry']['SurveyID'], name: survey['SurveyEntry']['SurveyName'])
+
+    # Selects all elements with element 'SQ'..
+    q = survey['SurveyElements'].select {|a| a['Element'] ==  'SQ'}
+
+    # iterate through all the 'SQ' elements..
+    q.each do |question|
+
+      # Creates the question from the element..
+      inQuestion = Question.create(qid: question['PrimaryAttribute'], title: question['SecondaryAttribute'], survey_id: inSurvey.id)
+
+      # Some questions have empty fields, condition to separate goes here..
+      if question['Payload']['Choices'].nil?
+        Answer.create(input: '', question_id: inQuestion.id)
+      else
+        question['Payload']['Choices'].each do |answer|
+          Answer.create(input: answer.last['Display'], question_id: inQuestion.id)
+        end
+      end
+    end
+    redirect_to root_path
+  end
 end
