6c613eea0c166243ba9ed4e2a77c32cfec3bc689
qwerqy <amnrsln@gmail.com>
2018-11-08 17:20:35 +0800

    add dropdown button for uploader

--- app/javascript/components/Navbar.js
+++ app/javascript/components/Navbar.js
@@ -1,57 +1,76 @@
-import React from "react"
-import { Button, Form, Container, Menu, Input } from 'semantic-ui-react';
-import axios from 'axios';
+import React from "react";
+import { Dropdown, Icon, Grid, Button, Container, Menu, Input } from "semantic-ui-react";
+import axios from "axios";
+import Uploader from "./Uploader"
 class Navbar extends React.Component {
   state = {
     loading: false,
     results: []
-  }
+  };
 
-  handleChange = (e) => {
-    const value = e.target.value
-    this.props.onSearch(value)
-  }
+  handleChange = e => {
+    const value = e.target.value;
+    this.props.onSearch(value);
+  };
 
-  handleKeyUp = (e) => {
-    const value = e.target.value
-    if(e.which === 13) {
+  handleKeyUp = e => {
+    const value = e.target.value;
+    if (e.which === 13) {
       this.setState({
         loading: true
-      })
+      });
       if (value.length > 0) {
-        axios.get('/search', { params: { search: value } })
-        .then(res => {
-          this.setState({ loading: true })
-          this.props.onResults(res.data)
-        })
-        .catch(() => this.setState({ loading: false, results: [] }));
+        axios
+          .get("/search", { params: { search: value } })
+          .then(res => {
+            this.setState({ loading: true });
+            this.props.onResults(res.data);
+          })
+          .catch(() => this.setState({ loading: false, results: [] }));
       } else {
         this.setState({ loading: false, results: [] });
       }
     }
-  }
+  };
 
-  render () {
+  render() {
     return (
       <Container style={{ padding: 20 }}>
-        <Menu borderless fluid widths={2}>
-          <Menu.Item>
-            <Form as='form' action='/upload' method='POST' encType="multipart/form-data">
-              <input type='hidden' name="authenticity_token" value={this.props.auth} />
-              <Form.Field>
-                <label>Upload QSF file</label>
-                <input type='file' name='qsf' />
-              </Form.Field>
-              <Button type='submit'>Submit</Button>
-            </Form>
-          </Menu.Item>
-          <Menu.Item>
-            <Input fluid name='search' icon='search' placeholder='Search for questions..' value={this.props.search} onKeyUp={this.handleKeyUp} onChange={this.handleChange}/>
-          </Menu.Item>
-        </Menu>
+        <Grid>
+          <Grid.Row>
+            <Grid.Column width={3}>
+              <Dropdown
+                button
+                className='icon'
+                floating
+                fluid
+                labeled
+                icon='plus'
+                text='Add QSF'
+              >
+                <Dropdown.Menu>
+                  <Dropdown.Item>
+                    <Uploader auth={this.props.auth}/>
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Grid.Column>
+            <Grid.Column width={13}>
+              <Input
+                fluid
+                name="search"
+                icon="search"
+                placeholder="Search for questions.."
+                value={this.props.search}
+                onKeyUp={this.handleKeyUp}
+                onChange={this.handleChange}
+                />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
       </Container>
     );
   }
 }
 
-export default Navbar
+export default Navbar;
---
+++ app/javascript/components/Uploader.js
@@ -0,0 +1,20 @@
+import React from "react"
+import { Form, Button } from "semantic-ui-react"
+class Uploader extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <Form as='form' action='/upload' method='POST' encType="multipart/form-data">
+          <input type='hidden' name="authenticity_token" value={this.props.auth} />
+          <Form.Field>
+            <label>Upload QSF file</label>
+            <input type='file' name='qsf' />
+          </Form.Field>
+          <Button type='submit'>Submit</Button>
+        </Form>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Uploader
